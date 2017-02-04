c779b4277b762ff2f833e34986482553084b3f89
Atul Rathore <kshtriya.atul@gmail.com>
2017-02-04 15:06:09 +0530

    Commit made with crash stop.

---
+++ app/src/main/java/com/example/atul/basicphrases/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.atul.basicphrases;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    protected void playAudio(View view) {
+        int id = view.getId();
+        String ourId = "";
+        ourId = view.getResources().getResourceEntryName(id);
+        int resourceId = getResources().getIdentifier(ourId, "raw", "com.example.atul.basicphrases");
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, resourceId);
+        mediaPlayer.start();
+        /*mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.release();
+            }
+        });*/
+        Log.i("Button Pressed", ourId);
+    }
+}
